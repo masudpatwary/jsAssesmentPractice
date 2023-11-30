169cc092b820e92a2a0423f3e5ffdbb025564fc7
Masudur Rahman <115950428+masudpatwary@users.noreply.github.com>
2023-11-30 16:47:44 +0600

    update

---
+++ object.js
@@ -0,0 +1,9 @@
+// ১২. একটা অবজেক্ট ডিক্লেয়ার করবে। সেটাতে তিনটা প্রপার্টি থাকবে। এবং কোন একটা প্রোপার্টি এর মান চেইঞ্জ করবা।
+
+const myFriends = {
+  name: "Kamal",
+  age: 25,
+  education: "gaduated",
+};
+myFriends.age = 50;
+console.log(myFriends);
